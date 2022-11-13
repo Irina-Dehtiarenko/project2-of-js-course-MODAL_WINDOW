d727b97e1546d89b803e218bfe5f5b5bf446f97c
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-11-13 16:47:54 +0100

    created the function of opening the modal window

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(modal, overlay, btnCloseModal, btnsOpenModal);
+
+const openModalWindow = e => {
+  console.log(e.target.textContent);
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+btnsOpenModal.forEach(btnCloseModal => {
+  btnCloseModal.addEventListener('click', openModalWindow);
+});
